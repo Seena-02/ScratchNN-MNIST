b1245c131987eb89b47bd39d9420ef25d247184c
Seena Mohajeran <seenamohajeran@gmail.com>
2024-05-19 23:37:48 -0700

    working nn

--- src/main.py
+++ src/main.py
@@ -3,41 +3,18 @@ import numpy as np
 import pandas as pd
 from matplotlib import pyplot as plt
 
-# Import data as pandas df
-data = pd.read_csv("../datasets/mnist_train.csv")
-
-# Convert pandas df to numpy array
-data = np.array(data)
-m, n = data.shape
-
-# Split data into dev and training sets to prevent overfitting 
-np.random.shuffle(data)
-
-# Use first 1000 examples for the validation(dev) set
-data_dev = data[0:1000].T
-Y_dev = data_dev[0]
-X_dev = data_dev[1:n]
-
-# Use remaining examples for training
-data_train = data[1000:m].T
-Y_train = data.train[0]
-X_train = data.train[1:n]
-
-
 def init_params():
     W1 = np.random.rand(10, 784) - 0.5
     b1 = np.random.rand(10, 1) - 0.5
     W2 = np.random.rand(10, 10) - 0.5
     b2 = np.random.rand(10, 1) - 0.5
-    
     return W1, b1, W2, b2
 
 def ReLU(Z):
      return np.maximum(0, Z)
 
 def softmax(Z):
-    return np.exp(Z) / np.sum(np.exp(Z))
-
+    return np.exp(Z) / sum(np.exp(Z))
 
 def forward_prop(W1, b1, W2, b2, X):
     Z1 = W1.dot(X) + b1
@@ -47,23 +24,23 @@ def forward_prop(W1, b1, W2, b2, X):
     return Z1, A1, Z2, A2
 
 def one_hot(Y):
-    one_hot_Y = np.zeros((Y.size(), Y.max() + 1))
-    one_hot_Y[np.arrange(Y.size), Y] = 1
+    one_hot_Y = np.zeros((Y.size, Y.max() + 1))
+    one_hot_Y[np.arange(Y.size), Y] = 1
     one_hot_Y = one_hot_Y.T
     return one_hot_Y
 
 def deriv_ReLU(Z):
     return Z > 0
 
-def back_prop(Z1, A1, Z2, A2, W2, X, Y):
+def back_prop(Z1, A1, Z2, A2, W1, W2, X, Y):
     m = Y.size
     one_hot_Y = one_hot(Y)
     dZ2 = A2 - one_hot_Y
     dW2 = 1 / m * dZ2.dot(A1.T)
-    db2 = 1 / m * np.sum()
+    db2 = 1 / m * np.sum(dZ2)
     dZ1 =  W2.T.dot(dZ2) * deriv_ReLU(Z1)
     dW1 = 1 / m * dZ1.dot(X.T)
-    db1 = 1 / m * np.sum(dZ1, 2)
+    db1 = 1 / m * np.sum(dZ1)
     return dW1, db1, dW2, db2
 
 def update_params(W1, b1, W2, b2, dW1, db1, dW2, db2, alpha):
@@ -73,5 +50,50 @@ def update_params(W1, b1, W2, b2, dW1, db1, dW2, db2, alpha):
     b2 = b2 - alpha * db2
     return W1, b1, W2, b2
 
+def get_predictions(A2):
+    return np.argmax(A2, 0)
 
+def get_accuracy(predictions, Y):
+    print(predictions, Y)
+    return np.sum(predictions == Y) / Y.size
+
+def gradient_decent(X, Y, iterations, alpha):
+    step = 10
+    W1, b1, W2, b2 = init_params()
+    for i in range(iterations + step):
+        Z1, A1, Z2, A2 = forward_prop(W1, b1, W2, b2, X)
+        dW1, db1, dW2, db2 = back_prop(Z1, A1, Z2, A2, W1, W2, X, Y)
+        W1, b1, W2, b2 = update_params(W1, b1, W2, b2, dW1, db1, dW2, db2, alpha)
+        if i % step == 0:
+            accuracy = get_accuracy(get_predictions(A2), Y)
+            print("Iteration: ", i)
+            print("Accuracy", round(accuracy * 100, 2), "%")
+    return W1, b1, W2, b2
+
+if __name__ == "__main__":
+    # Import data as pandas df
+    data = pd.read_csv("../datasets/mnist_train.csv")
+
+    # Convert pandas df to numpy array
+    data = np.array(data)
+    m, n = data.shape
+
+    # Split data into dev and training sets to prevent overfitting 
+    np.random.shuffle(data)
+
+    # Use first 1000 examples for the validation(dev) set
+    data_dev = data[0:1000].T
+    Y_dev = data_dev[0]
+    X_dev = data_dev[1:n]
+    X_dev = X_dev / 255.
+
+    # Use remaining examples for training
+    data_train = data[1000:m].T
+    Y_train = data_train[0]
+    X_train = data_train[1:n]
+    X_train = X_train / 255.
+    _,m_train = X_train.shape
+
+    # Begin gradient decent
+    W1, b1, W2, b2 = gradient_decent(X_train, Y_train, 500, 0.1)
 
